268ac964d9d1cab9fc6b18da9fe145d8442009c6
lindanur <lindanuranisaa@gmail.com>
2018-03-17 11:51:45 +0700

    push pertama kali

---
+++ MainProject-master/app/src/main/java/id/simplify/prosperoinv/tampilan_stock.java
@@ -0,0 +1,13 @@
+package id.simplify.prosperoinv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tampilan_stock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan_stock);
+    }
+}
